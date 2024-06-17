331f2cdbee631707c370ef58d177cec27aeec08c
Noe Ulises Machaca Chambilla <105571999+newneo4@users.noreply.github.com>
2024-06-16 23:56:42 -0500

    Update model.py

--- model.py
+++ model.py
@@ -3,10 +3,8 @@ import streamlit as st
 from tensorflow.keras.models import load_model
 import pickle
 
-# Carga el modelo
 model = load_model("modelos/sql_injection_model2.h5")
 
-# Carga el vectorizador
 with open("modelos/vectorizer.pkl", "rb") as file:
     vectorizer = pickle.load(file)
 
@@ -16,22 +14,16 @@ predicciones = {
 }
 
 def predict_query(query):
-    # Preprocesar la consulta SQL utilizando el vectorizador cargado
     query_vectorized = vectorizer.transform([query])
     
-    # Convertir el vectorizado a un formato que el modelo pueda usar
     query_array = query_vectorized.toarray()
     
-    # Ajustar la forma para que coincida con la entrada del modelo (None, 1, 10321)
     query_array = query_array.reshape(-1, 1, query_array.shape[1])
     
-    # Realizar predicciones con el modelo cargado
     predictions = model.predict(query_array)
     
-    # Obtener el índice de la clase predicha
     indice_maximo = np.argmax(predictions)
     
-    # Obtener la etiqueta de predicción
     prediction_label = predicciones[indice_maximo]
     
     return prediction_label
